0ba26f27b10b04b3c389c8c6dc88aeca720094fa
Renaud <renaud.balat@hotmail.com>
2018-03-07 14:26:56 +0100

    affichage du stockage de la carte sd à l'accueil

--- index.php
+++ index.php
@@ -1,15 +1,19 @@
 <?php require "header.php"; ?>
 
         <section class="col-sm-10">
-          <div class="panel panel-default">
-            <div class="panel-heading">
-              <h3 class="panel-title">Bienvenue sur votre tableau de bord !</h3>
-            </div>
-            <div class="panel-body">
-		<p>Ici sera présenté toutes les options du sites.</p>
-		<p>Et un petit test de git !!</p>		
-            </div>
-          </div>
+			<div class="panel panel-default">
+				<div class="panel-heading">
+					<h3 class="panel-title">Bienvenue sur votre tableau de bord !</h3>
+				</div>
+				<div class="panel-body">
+		<?php 
+		$date = date("Y-m-d");
+		$query = 'SELECT * FROM stockage WHERE date=\''.$date.'\'';
+		$rep = $bdd->query($query)->fetch();
+		
+		echo $rep['date'].' : '.$rep['utilise'].'G utilisé & '.$rep['disponible'].'G disponible';?>
+				</div>
+			</div>
         </section>
 
 <?php include('footer.php'); ?>
--- taille_disk.sh
+++ taille_disk.sh
@@ -4,15 +4,20 @@
 df -h | head -n 2 | tail -n 1 > ligne
 cut -c28-37 ligne > taille
 sed -e 's/G//g' taille > valeur
-sed -e 's/  / /g' valeur > valeur2
+sed -e 's/,/./g' valeur > valeur1
+sed -e 's/  / /g' valeur1 > valeur2
 sed 'y, ,\n,' valeur2 > val
 
 utilise=$(head -n 1 val)
 disponible=$(tail -n 1 val)
-echo 'taille utilise : ' ${utilise}
-echo 'taille disponible : ' ${disponible}
+#echo 'taille utilise : ' ${utilise}
+#echo 'taille disponible : ' ${disponible}
+DATE=$(date +%Y-%m-%d)
+echo "INSERT INTO stockage (id, date, disponible, utilise) VALUES ('', '$DATE', '$disponible', '$utilise');" | mysql -uadmin -prenaud vehicule;
+
 rm ligne
 rm taille
 rm valeur
+rm valeur1
 rm valeur2
 rm val
